84a104ea1a40dba7db50e5c186f88044591046a0
hehe hehe <ragux0@gmail.com>
2026-04-19 22:11:44 +0200

    Fix lint issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,30 @@
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
 
+const sharedRules = {
+  ...tseslint.configs.recommended.rules,
+  "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+};
+
+const sharedPlugins = { "@typescript-eslint": tseslint };
+
 export default [
   {
-    files: ["src/**/*.ts", "scripts/**/*.ts"],
+    files: ["src/**/*.ts"],
     languageOptions: {
       parser: tsParser,
       parserOptions: { project: "./tsconfig.json" },
     },
-    plugins: { "@typescript-eslint": tseslint },
-    rules: {
-      ...tseslint.configs.recommended.rules,
-      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    plugins: sharedPlugins,
+    rules: sharedRules,
+  },
+  {
+    files: ["scripts/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: { project: "./tsconfig.scripts.json" },
     },
+    plugins: sharedPlugins,
+    rules: sharedRules,
   },
 ];
